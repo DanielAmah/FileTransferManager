46d830a96ec374a60078b4011706d1c888037640
DanielAmah <amahdanieljack@gmail.com>
2018-09-04 16:44:49 +0100

    add validation to file upload

--- app/models/file_upload.rb
+++ app/models/file_upload.rb
@@ -1,3 +1,22 @@
 class FileUpload < ApplicationRecord
   has_many_attached :files
+  validates :name, presence: true
+  validate :file_attached
+
+
+  private
+
+  def file_attached
+    if files.attached? == false
+      files.purge_later
+      errors[:base] << 'File is not attached'
+    elsif files.attached?
+      files.each do |file|
+        if file.byte_size > 10485760
+          file.purge
+          errors[:base] << 'File is too large. It has to be below 10MB'
+        end
+      end
+    end
+  end
 end
